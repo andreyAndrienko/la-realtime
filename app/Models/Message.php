8005af2dc91f9a08a96977852c8274bf11528e76
Andrey Andrienko <andrey_fist@inbox.ru>
2020-03-15 17:54:30 +0200

    Message model

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Message
+ *
+ * @property int $id
+ * @property string $message
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereMessage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Message whereUserId($value)
+ * @mixin \Eloquent
+ */
+class Message extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'message'
+    ];
+}
